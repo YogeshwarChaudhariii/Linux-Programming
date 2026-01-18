d034e6f3975387ef2158e729b45a3628e59d978b
Yogeshwar Chaudhari <108257281+YogeshwarChaudhariii@users.noreply.github.com>
2026-01-18 17:55:40 +0530

    Update open().c

--- C Language/File Handling/open().c
+++ C Language/File Handling/open().c
@@ -1,1 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+    FILE *fptr;
+
+    char data[] = "Jay Ganesh";
+
+    fptr = fopen("demo.txt", "w");
+
+    if (fptr == NULL)
+    {
+        printf("Error opening file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(fptr, "%s", data);
+
+    fclose(fptr);
+
+    printf("Data written to demo.txt\n");
+
+    return 0;
+}
